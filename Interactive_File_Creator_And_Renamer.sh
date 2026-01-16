6fe6e8acaa11f2682d0424b23ccc4862c2bcc7a5
ShikhaSingh1807 <shikha1807@gmail.com>
2026-01-16 19:41:27 +0530

    MileStone-2 creating a separate log file

--- Interactive_File_Creator_And_Renamer.sh
+++ Interactive_File_Creator_And_Renamer.sh
@@ -1,10 +1,29 @@
 #!/bin/bash
 
+#Making a directory which will contain log files
+mkdir logs
+
+#Initializing a log file 
+LOG_FILE="logs/script_log.txt"
+
+#Creating a log function which will take as input a string which is message and print it in the log file
+log()
+{
+	echo "$(date '+%Y-%m-%d %H:%M:%S ') - $1 " >> $LOG_FILE
+}
+
+
+
 #Asking user how many txt and jpeg file he/she wants to create 
 #read command used to take input and -p helps in printing message while asking input
 
 read -p "How many .txt files needs to be created : " txtNum
 read -p "How many .jpeg files needs to be created : " jpegNum
+
+#Adding the details in log
+log "How many .txt files needs to be created : $txtNum"
+log "How many .jpeg files needs to be created : $jpegNum"
+log "***********************************************"
 
 # We will use for loop to create file with name as num.txt for eg :- 1.txt ,2.txt
 
@@ -26,7 +45,15 @@ echo "Creating File...."
 sleep 2s
 
 #Post files created print the content of the Current working directory
-ls -lrt .
+#Storing the result in a variable so that it can be added in logger as well
+
+result=$(ls -lrt .)
+
+echo "$result"
+
+log "$result"
+
+log "**********************************************************************"
 
 #Asking user to input 't' for renaming text files and 'j' to rename jpeg files . 
 #If user type any other character or string then user will be kept prompted to enter the choice
@@ -34,17 +61,23 @@ ls -lrt .
 
 read -p " Type t if want to rename .txt files , Type j if want to rename .jpeg file : " rename
 
+log " Type t if want to rename .txt files , Type j if want to rename .jpeg file : "
+
 #Creating while loop to keep user prompting in case ofwrong input
 while [[ $rename != 'j' && $rename != 't' ]]
 do
     echo "Typed : $rename"
+    log "Typed : $rename"
     read -p " Type t if want to rename .txt files , Type j if want to rename .jpeg file : " rename
+    log " Type t if want to rename .txt files , Type j if want to rename .jpeg file : "
+    log "********************************************************************************"
 done
 
 #User outside while loop means inputted properly t or j
 read -p " Enter the prefix you want to add to the file names : " prefix
 
 echo " Prefix is : $prefix"
+log " Prefix is : $prefix"
 
 # if condition to decide the extension of the file based on the input character
 if [[ $rename == 'j' ]]
@@ -53,6 +86,9 @@ then
 else
     extension=".txt"
 fi
+
+echo "Extension is : $extension"
+log "Extension is : $extension"
 
 #if condition to check the input and acordingly rename file using for loop to interate over all those 
 # type of file
@@ -73,5 +109,11 @@ fi
 sleep 0.5s
 echo "Files Renamed..."
 
-ls -lrt .
+log "**************************************************************************"
+result=$(ls -lrt .)
 
+echo "$result"
+
+log "$result"
+
+log "**********************************************************************"
